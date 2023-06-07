bf07b1c1af6d8813d1495f9d7613885023beefee
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-06-07 17:23:55 +0700

    learn JS - object.method.mutation

--- 00-js-basics/mutation.js
+++ 00-js-basics/mutation.js
@@ -2,10 +2,23 @@ const person = {
     name: 'Bob',
     age: 25
 }
-
+//variant 1
 const person2 = Object.assign({}, person)
 
-person2.age = 26
+//variant 2
+const person3 = {...person}
 
-console.log(person2.age)
-console.log(person.age)
+//variant 3
+const person4 = JSON.parse(JSON.stringify(person))
+
+person2.age = 26
+person2.name = 'Alice'
+person3.age = 29
+person3.name = 'Wiliam'
+person4.age = 33
+person4.name = 'Wiliy'
+
+console.log(person)
+console.log(person2)
+console.log(person3)
+console.log(person4)
