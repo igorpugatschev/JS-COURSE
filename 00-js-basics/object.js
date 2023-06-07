8f5ab4add8f95fc3fdc6e81e8d4cbc06ceea1b98
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-06-07 14:10:23 +0700

    learn JS - object

--- 00-js-basics/object.js
+++ 00-js-basics/object.js
@@ -3,8 +3,13 @@ const myCity = {
 }
 console.log(myCity)
 
+//добавление свойств в объект
 myCity.popular = true
 console.log(myCity)
 
 myCity.country = 'USA'
+console.log(myCity)
+
+//удаление свойств объекта
+delete myCity.country
 console.log(myCity)
