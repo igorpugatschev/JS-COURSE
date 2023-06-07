1f879937f29163bbc3430640b9a7a41480874916
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-06-07 16:06:51 +0700

    learn JS - object injection

--- 00-js-basics/object2.js
+++ 00-js-basics/object2.js
@@ -13,3 +13,20 @@ console.log(myCity.info.country)
 //delete myCity.info['isPopular']
 delete myCity.info.isPopular
 console.log(myCity)
+
+const name = 'Bogdan'
+const postsQty = 23
+/**
+const userProfile = {
+    name: name,
+    postsQty: postsQty,
+    hasSignedAgreement: false
+}
+*/
+const userProfile = {
+    name,
+    postsQty,
+    hasSignedAgreement: false
+}
+
+console.log(userProfile)
